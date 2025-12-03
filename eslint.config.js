b936e4e4fa1df734bb0416daf326fa899fbdf692
shengq <shengq007@outlook.com>
2025-12-03 16:39:50 +0800

    feat: eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -8,6 +8,7 @@ export default antfu(
 	},
 	{
 		rules: {
+			'style/member-delimiter-style': 'off',
 			// 箭头函数的参数是否必须用圆括号 () 包裹
 			'style/arrow-parens': ['error', 'as-needed'],
 			'style/jsx-one-expression-per-line': 'off',
